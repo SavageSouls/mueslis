cc5729bd24a68078d2c2fc7f72dfdd5e41ea1821
Varga Botond <btnd.varga@gmail.com>
2025-09-23 11:50:22 +0200

    Apply style

---
+++ react-app/src/pages/MueslisPage.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+import '../App.css';
+
+export default class MueslisPage extends Component{
+    render(){
+        return(
+            <div>
+                <main id="content-area">
+                  {/* Default content is Products/Prices table */}
+                  <h1>My mueslis</h1>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.muesliData.result.map(muesli=><tr key={muesli.id}>
+                        <td>{muesli.id}</td>
+                        <td>{muesli.name}</td>
+                        <td>{muesli.price}</td>
+                      </tr>)}
+                    </tbody>
+                  </table>
+                </main>
+            </div>
+        )
+    }
+
+    async componentDidMount(){
+    try{
+      let muesliData = await fetch('http://localhost:3333/mueslis')
+      console.log('muesliData', muesliData)
+      muesliData = await muesliData.json()
+      console.log('muesliData', muesliData)
+
+      this.setState({muesliData})
+    } catch(e){
+      console.warn(e)
+    }
+    
+  }
+}
